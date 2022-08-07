13049be471d80d88f5e7f93b1d6f4f397b59008b
Ahmed Zaki <supremedr22@yahoo.com>
2022-08-07 07:40:51 +0200

    commit

---
+++ test.py
@@ -0,0 +1,7 @@
+x="""    Blog Posts is a blogging platform where you create your personal blog and share your ideas with the world.
+
+    The project was a website for local group of young writers. create your own blog with the name you like, no one will know your account name, except for the admin. You can share any of your posts with other blogs you choose.
+
+     I worked on the backend side with Django python framework, with generic views and Django's authentication system. An admin could manage the website content through admin website delivered out of the box with django.""".split()
+
+print (x)
