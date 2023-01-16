a580a6bca93bb33bd7d1f4754cfbfde507c5d577
Ahmed Zaki <supremedr22@yahoo.com>
2023-01-16 20:35:40 +0200

    deploy to railway

--- blueblog/settings/base.py
+++ blueblog/settings/base.py
@@ -29,7 +29,7 @@ def get_env_variable(var_name):
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_env_variable('SECRET_KEY')
+SECRET_KEY = get_env_variable('secret_key')
 
 
 ALLOWED_HOSTS = []
