caee446cb4cf4436fff5ce51bce186724e09cfb3
Ahmed Zaki <supremedr22@yahoo.com>
2023-01-16 17:24:21 +0200

    new commit

--- blueblog/settings/base.py
+++ blueblog/settings/base.py
@@ -29,7 +29,7 @@ def get_env_variable(var_name):
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_env_variable('secret_key')
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 
 ALLOWED_HOSTS = []
